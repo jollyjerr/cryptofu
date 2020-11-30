3f6d54cc0690566a01992c8d060a19818fa97ff3
jollyjerr <jollyjerr@gmail.com>
2020-11-30 07:53:43 -0700

    :robot: Add slackbot

---
+++ bot/slackbot.go
@@ -0,0 +1,70 @@
+package bot
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	httpClient = http.Client{
+		Timeout: time.Second * 30,
+	}
+)
+
+// SendSlackFinancials is for buy and sell logging only
+func SendSlackFinancials(message interface{}) error {
+	_, err := post("https://hooks.slack.com/services/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", message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// SendSlackLogging sends general logs
+func SendSlackLogging(message interface{}) error {
+	_, err := post("https://hooks.slack.com/services/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", message)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func post(url string, body interface{}) (*http.Response, error) {
+	marshaledBody, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	message := map[string]interface{}{
+		"text": string(marshaledBody),
+	}
+	marshaledBody, err = json.Marshal(message)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(marshaledBody))
+	if err != nil {
+		return nil, err
+	}
+
+	addStandardHeaders(req)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("Status Code: %d", resp.StatusCode)
+	}
+
+	return resp, nil
+}
+
+func addStandardHeaders(req *http.Request) {
+	req.Header.Set("User-Agent", "cryptofu")
+	req.Header.Set("Accept", "application/json")
+}
